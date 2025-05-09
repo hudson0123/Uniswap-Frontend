3caead1e0c1ad81e0c8f74c5c4aded0c4a1aed38
Hudson O'Donnell <hudsonodonnell2004@gmail.com>
2025-05-09 15:53:42 -0400

    update login form

--- src/components/Forms/LoginForm.tsx
+++ src/components/Forms/LoginForm.tsx
@@ -5,7 +5,7 @@ interface LoginFormProps {
     login: (username: string, password: string) => void
 }
 
-const LoginFormSchema  = z.object({
+const LoginFormSchema = z.object({
     username: z.string(),
     password: z.string(),
 })
@@ -44,23 +44,29 @@ export default function LoginForm({ login }: LoginFormProps) {
 
     return (
         <form onSubmit={handleSubmit} className='grid grid-cols-1 px-8 py-10 rounded text-black '>
-            <label className='mt-4 mb-2' htmlFor="username">Username</label>
-            <input
-                id="username"
-                type="text"
-                value={form.username}
-                onChange={handleChange}
-                className='bg-gray-200 text-black rounded px-2 py-1'
-            />
-            <label className='mt-4 mb-2' htmlFor="password">Password</label>
-            <input
-                id="password"
-                type="password"
-                value={form.password}
-                onChange={handleChange}
-                className='bg-gray-200 text-black rounded px-2 py-1'
-            />
-            <button className="border bg-black text-white rounded-md py-2 w-1/2 mt-5 h-10 hover:opacity-80 cursor-pointer transform duration-200" type="submit">Submit</button>
+            <div className='relative mt-8'>
+                <input
+                    id="username"
+                    type="text"
+                    value={form.username}
+                    onChange={handleChange}
+                    placeholder=" "
+                    className='block px-2.5 pb-2.5 pt-4 w-full text-sm bg-gray-300 rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 peer'
+                />
+                <label className="absolute text-sm duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-4 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Username</label>
+            </div>
+            <div className='relative mt-8'>
+                <input
+                    id="password"
+                    type="password"
+                    value={form.password}
+                    onChange={handleChange}
+                    placeholder=" "
+                    className='block px-2.5 pb-2.5 pt-4 w-full text-sm bg-gray-300 rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 peer'
+                />
+                <label className="absolute text-sm duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-4 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Password</label>
+                </div>
+            <button className="border bg-black text-white rounded-md py-2 w-1/2 mt-5 h-10 hover:opacity-80 cursor-pointer transform duration-100 focus:opacity-70" type="submit">Submit</button>
         </form>
     );
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -8,7 +8,7 @@ export default function Login() {
     const login = useAuthStore((state) => state.login)
 
     return (
-        <div className='flex flex-col h-screen justify-center items-center bg-white'>
+        <div className='flex flex-col h-screen justify-center items-center bg-[#f5f5f5]'>
             <h2 className='text-black text-2xl font-bold'>Login to UniSwap</h2>
             <LoginForm login={login} />
         </div>
