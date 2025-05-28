3b813a38823542a5b2314a47e63f72b60fa4b981
Hudson O'Donnell <hudsonodonnell2004@gmail.com>
2025-05-28 14:59:17 -0400

    banner and pfp on account page

--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -4,6 +4,7 @@ import { useAuthStore } from '@/lib/store';
 import { useQuery } from '@tanstack/react-query';
 import api from '@/lib/api';
 import { IUser } from '@/@types';
+import Image from 'next/image';
 
 export default function AccountPage() {
     const router = useRouter();
@@ -37,11 +38,16 @@ export default function AccountPage() {
     }
 
     return (
-        <div>
-            <div className=''>
-                <p className='italics'>@{account_data?.username}</p>
-                <p className='ml-auto'>{account_data?.first_name} {account_data?.last_name}</p>
+        <div className='flex flex-col h-screen bg-white text-black'>
+            <div className='relative w-screen'>
+                <Image src="/banner.jpg" width={100} height={100} alt="banner" className='w-screen h-30 object-fill'/>
+                <Image src={account_data.profile_picture ? account_data.profile_picture : "/profile.jpg"} width={100} height={100} alt="profile" className='w-30 h-30 flex absolute left-3/5 -bottom-10 rounded-full border-1 border-black shadow-2xl bg-white'/>
             </div>
+            <div className='flex flex-col gap-3 justify-center align-middle mr-auto ml-auto mt-15 text-center'>
+                <p className='text-xl font-bold'>{account_data?.first_name} {account_data?.last_name}</p>
+                <p className='italics'>@{account_data?.username}</p>
+            </div>
+            
         </div>
     );
 }
