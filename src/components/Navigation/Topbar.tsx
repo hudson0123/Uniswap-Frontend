2c1b5eb08cb6eb6ae47d09dbd3f9a550076f8efc
Hudson O'Donnell <hudsonodonnell2004@gmail.com>
2025-06-26 16:17:25 -0400

    resolved the following issues by allowing users to click directly on avatar picture to go to account
    In mobile users need to be able to enter the account section by clicking profile

--- src/components/Navigation/Topbar.tsx
+++ src/components/Navigation/Topbar.tsx
@@ -11,30 +11,31 @@ function Topbar() {
   const profileImage = currentUserData?.profile_picture || "/profile.jpg";
 
   return (
-      <nav className="w-full bg-white shadow-sm z-10 py-4 h-[9vh]">
-        <div className="max-w-screen-3xl mx-auto flex items-center justify-between px-4 sm:px-10 h-full">
-          {/* Logo */}
-          <Link href="/home" className="text-2xl font-semibold text-black">
-            <Image
-              src="/uniswap.png"
-              alt="Chat"
-              width={150}
-              height={150}
-              className="mt-2 bg-transparent transition duration-150 hover:scale-105"
-            />
+    <nav className="w-full bg-white shadow-sm z-10 py-4 h-[9vh]">
+      <div className="max-w-screen-3xl mx-auto flex items-center justify-between px-4 sm:px-10 h-full">
+        {/* Logo */}
+        <Link href="/home" className="text-2xl font-semibold text-black">
+          <Image
+            src="/uniswap.png"
+            alt="Chat"
+            width={150}
+            height={150}
+            className="mt-2 bg-transparent transition duration-150 hover:scale-105"
+          />
+        </Link>
+
+        {/* Right-side icons */}
+        <div className="flex items-center gap-4">
+          {/* Chat Icon */}
+          <Link href="/chat" className="cursor-pointer">
+            <h2 className="font-sans mt-2 hover:text-gray-500 text-lg mr-5">
+              Messages
+            </h2>
           </Link>
 
-          {/* Right-side icons */}
-          <div className="flex items-center gap-4">
-            {/* Chat Icon */}
-            <Link href="/chat" className="cursor-pointer">
-              <h2 className="font-sans mt-2 hover:text-gray-500 text-lg mr-5">
-                Messages
-              </h2>
-            </Link>
-
-            {/* Profile */}
-            <div className="relative group">
+          {/* Profile */}
+          <div className="relative group">
+            <Link href={`/${currentUserData?.username}/`}>
               <Image
                 src={profileImage}
                 alt="User profile"
@@ -42,11 +43,12 @@ function Topbar() {
                 height={48}
                 className="w-12 h-12 rounded-full hover:ring-2 cursor-pointer transition duration-150"
               />
-              <TopbarProfileDropdown />
-            </div>
+            </Link>
+            <TopbarProfileDropdown />
           </div>
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 }
 
