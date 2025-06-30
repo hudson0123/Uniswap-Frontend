22dff300f680093e588a57f91f880378ce4ab113
Hudson O'Donnell <hudsonodonnell2004@gmail.com>
2025-06-30 15:49:40 -0400

    basic cusom auth provider

--- src/providers/Auth.tsx
+++ src/providers/Auth.tsx
@@ -1,18 +1,21 @@
+import { useAuthStore } from '@/lib/store';
 import { useRouter } from 'next/router';
 import { PropsWithChildren, useEffect } from 'react'
 
 export default function AuthProvider({children}: PropsWithChildren) {
 
   const router = useRouter();
-
-
+  const access = useAuthStore((state) => state.access);
+  
+  
   useEffect(() => {
     const currentPath = router.pathname;
+    const protectedRoutes = currentPath.startsWith('/app') || currentPath.startsWith('/auth/verify')
   
-    if (currentPath.startsWith('/app') || currentPath.startsWith('/auth/verify')) {
+    if (!access && protectedRoutes) {
       router.push('/auth/login');
     }
-  }, [router])
+  }, [router, access])
 
   return children;
 }
