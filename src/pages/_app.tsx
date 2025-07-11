fb2ab9fb53454c82b5085d8e58520bde422b6463
Hudson O'Donnell <hudsonodonnell2004@gmail.com>
2025-07-11 13:33:20 -0400

    remove NotificationBanner component import and usage from App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import WebSocketProvider from "@/providers/WebSocketProvider";
-import NotificationBanner from "@/components/NotificationBanner";
 import AuthProvider from "@/providers/Auth";
 import SingleButtonModal from "@/components/Modals/SingleButtonModal";
 import { useModalStore } from "@/lib/store";
@@ -52,7 +51,6 @@ export default function App({ Component, pageProps }: AppProps) {
             />
           )}
           <Component {...pageProps} />
-          <NotificationBanner />
         </WebSocketProvider>
       </AuthProvider>
       <Toaster 
